99c10bcef0c25c0c72964ecc3a7b5a40c370e076
setbeable <65809334+setbeable@users.noreply.github.com>
2025-08-31 22:43:35 +0200

    Revert "31_ago"
    This reverts commit 72b648bb855edd32653661fba7c82f15a855ead4.

--- js/foto.js
+++ js/foto.js
@@ -1,4 +1,1 @@
-// Galleria: aggiungi i file nella cartella /documenti/foto e lista qui i nomi
-const IMMAGINI=[ "partita1.jpg", "premiazione.jpg" ];
-const gal=document.getElementById('galleria'); if(gal){ gal.classList.add('gallery');
-IMMAGINI.forEach((n,i)=>{ const a=document.createElement('a'); a.href="../documenti/foto/"+n; a.dataset.index=i; const img=document.createElement('img'); img.src="../documenti/foto/"+n; img.alt=n; a.appendChild(img); gal.appendChild(a); }); }
+const IMMAGINI=["partita1.jpg","premiazione.jpg"];const gal=document.getElementById('galleria');gal.classList.add('gallery');IMMAGINI.forEach(n=>{const i=document.createElement('img');i.src="../documenti/foto/"+n;i.alt=n;gal.appendChild(i);});
--- js/radial-export-import.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  const STORAGE_KEY='radial_layout_v3';
-  const exp=document.getElementById('export-layout');
-  const imp=document.getElementById('import-layout');
-  if(exp){ exp.addEventListener('click', function(){ const data=localStorage.getItem(STORAGE_KEY)||"{}"; const blob=new Blob([data],{type:'application/json'}); const a=document.createElement('a'); a.href=URL.createObjectURL(blob); a.download='layout-radiale.json'; document.body.appendChild(a); a.click(); a.remove(); });}
-  if(imp){ imp.addEventListener('change', function(){ const f=imp.files[0]; if(!f) return; const r=new FileReader(); r.onload=function(){ try{ JSON.parse(r.result); localStorage.setItem(STORAGE_KEY, r.result); location.reload(); }catch(e){ alert('File non valido'); } }; r.readAsText(f); });}
-})();
